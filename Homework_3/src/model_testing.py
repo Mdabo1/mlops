a02faf53757280d116196c2d8a7229472d5f6c98
Nurislam Akbulatov <122386960+Mdabo1@users.noreply.github.com>
2024-05-10 19:00:20 +0500

    fix bug

--- Homework_3/src/model_testing.py
+++ Homework_3/src/model_testing.py
@@ -4,23 +4,26 @@ import joblib
 import pandas as pd
 
 # Load the test data and the model
-X_test = pd.read_csv("Homework_1\\test\\X_test_processed.csv", index_col=0)
-y_test = pd.read_csv("Homework_1\\test\\y_test_enc.csv", index_col=0)
+X_test = pd.read_csv("X_test_processed.csv", index_col=0)
+y_test = pd.read_csv("y_test_enc.csv", index_col=0)
 
-model = joblib.load("Homework_1\\linear_regression_model.pkl")
+model = joblib.load("linear_regression_model.pkl")
 y_pred = model.predict(X_test)
 
 loss = mean_squared_error(y_test, y_pred)
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to the FastAPI app"}
 
+
 @app.get("/evaluate")
 async def evaluate():
     return {"mean_squared_error": loss}
+
 
 @app.post("/predict")
 async def predict(data: dict):
@@ -30,5 +33,6 @@ async def predict(data: dict):
         return {"prediction": prediction.tolist()}
     except Exception as e:
         return {"error": str(e)}
+
 
 print(f"Initial evaluation MSE: {loss}")
